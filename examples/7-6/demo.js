a0f0d3450a80f80cc8f63602a449886259cc94e6
rubeewoo <rugoldfishwu@gmail.com>
2021-11-05 09:38:05 +0800

    7-6 java

---
+++ examples/7-6/demo.js
@@ -0,0 +1,27 @@
+// alert('This is an alert!');
+
+function printMsg() {
+	console.log('This is a message in the console');
+}
+
+printMsg();
+
+var varFunction = function() {
+	console.log('This is a function stored in a variable.');
+};
+
+varFunction();
+
+// --------------------------
+
+alwaysDoFirst();
+
+function alwaysDoFirst() {
+	console.log('You can call this function anywhere in the script!');
+}
+
+callThisLater();
+
+var callThisLater = function() {
+	console.log('You can only call this function after it is built.');
+};
