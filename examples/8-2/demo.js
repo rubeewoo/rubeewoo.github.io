aad22867063152132b2bdf823d367708e0016c9b
rubeewoo <rugoldfishwu@gmail.com>
2021-11-05 10:20:32 +0800

    numarray

---
+++ examples/8-2/demo.js
@@ -0,0 +1,10 @@
+var numArray = [1, 5, 2, 11, 7];
+
+console.log(numArray[0]); // First position
+console.log(numArray[1]); // Second position
+
+console.log(numArray[4]); // Last position
+numArray[4] = 18;
+console.log(numArray[4]); // Last position with a different value
+
+console.log(numArray); // Entire array
