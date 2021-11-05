764d8d1a83df81868615a0f197ee6fafb0a19662
rubeewoo <rugoldfishwu@gmail.com>
2021-11-05 10:03:44 +0800

    Update demo.js

--- examples/8-1/demo.js
+++ examples/8-1/demo.js
@@ -3,5 +3,23 @@ function printMsg(msg) {
 	console.log(finalMsg);
 }
 
-printMsg('Test!');
-printMsg('Another Test!');
+//printMsg('Test!');
+//printMsg('Another Test!');
+
+function userMsg((name, msg) {
+	let finalMsg = name + 'says:' + msg;
+	console.log(finalMsg);
+}
+
+//userMsg('Victor', 'Surprise!');
+//userMsg('Mary', 'I love Victor!');
+
+function complexCalc(x,y) {
+	let result = 0;
+	result = x*y;
+	result = result / (x - y);
+	result = result * y - x;
+	return result;
+}
+
+var calcResult = complexCalc(4, 6);
