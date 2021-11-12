c99f28c8eb4b6f36fc2928bcd4fc7cda83f323f0
rubeewoo <rugoldfishwu@gmail.com>
2021-11-12 10:04:54 +0800

    9-1

--- examples/9-1/demo.js
+++ examples/9-1/demo.js
@@ -5,5 +5,19 @@
 var winWidth = window.innerWidthh;
 var winHeight = window.innerHeight;
 
-console.log(winWidth);
-console.log(winHeight);
+//console.log(winWidth);
+//console.log(winHeight);
+
+//window.location.href = 'https://google.com';
+//window.location.pathname = '/nosuchpath';
+
+window.onload = function() {
+	//alert('Website is ready!');
+	//window.document: The entire HTML document
+	let siteTitle = document.title;
+	console.log(siteTitle);
+
+
+	let allDivs = document.getElementsByTagName('div');
+	console.log(allDivs[0]);
+};
