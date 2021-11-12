d06efb9a22c96f7606bccbbd7cf26b73c1e41d42
rubeewoo <rugoldfishwu@gmail.com>
2021-11-12 10:27:14 +0800

    Update demo.js

--- examples/9-1/demo.js
+++ examples/9-1/demo.js
@@ -19,5 +19,19 @@ window.onload = function() {
 
 
 	let allDivs = document.getElementsByTagName('div');
-	console.log(allDivs[0]);
+	//console.log(allDivs.length);
+	//console.log(allDivs[0]);
+	//console.log(allDivs[1].innerText);
+
+	let emById = document.getElementsById('div3');
+	//console.log(emById.innerText);
+
+	let classEms = document.getElementsByClassName('group1');
+	//console.log(classEms[0].innerText);
+
+	let emBySelector = document.querySelector('div > p');// First match only
+	//console.log(emBySelector.innertext);
+
+	let emsBySelector = document.querySelectorAll('div > p'); // All matches 
+	console.log(emsBySelector.length);
 };
