a66645f1caaf9c4b0adf56efbb3b832d6d275e49
rubeewoo <rugoldfishwu@gmail.com>
2021-11-12 09:35:22 +0800

    8-2

---
+++ examples/9-1/demo.js
@@ -0,0 +1,9 @@
+//console.log('Hi!');
+
+//window:The entire browser window
+
+var winWidth = window.innerWidthh;
+var winHeight = window.innerHeight;
+
+console.log(winWidth);
+console.log(winHeight);
