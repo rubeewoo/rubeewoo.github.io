45a7fce493c68c4899220987b57711f43cde1512
rubeewoo <rugoldfishwu@gmail.com>
2021-11-26 09:40:54 +0800

    jquery 11-1

--- examples/11-1/demo.js
+++ examples/11-1/demo.js
@@ -1,6 +1,8 @@
 window.onload = function() {
 	let button = document.querySelector('button');
-	button.addEventListener('click', function() {
+	// the 'event' information is passed over by the browser
+	button.addEventListener('click', function(event) {
+		event.stopPropagation(); // In the 'event' object, there's a stopPropagation function
 		alert('button clicked!');
 	});
 	let div = document.querySelector('div');
