3966cf6befa3eca9977af56f4cffe562f1fb3031
rubeewoo <rugoldfishwu@gmail.com>
2021-11-26 11:42:47 +0800

    11-5

---
+++ examples/11-5/demo.js
@@ -0,0 +1,18 @@
+window.onload = function() {
+	$('body > div').on('click', function() {
+		//Change one CSS property only
+		//$(this).css('background-color', 'red');
+
+		//Change multiple CSS properties
+		let newCSS = {
+			'background-color': 'red',
+			'font-size': '200%'
+		};
+		$(this).css(newCSS);
+	});
+
+	$('body > button').on('click', function(event) {
+		event.stopPropagation();
+		alert('button clicked!');
+	});
+}
