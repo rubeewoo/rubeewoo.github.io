f276799348bbf7778305ecafe35df5b9404a5a20
rubeewoo <rugoldfishwu@gmail.com>
2021-11-26 11:14:24 +0800

    Update demo.js

--- examples/11-4/demo.js
+++ examples/11-4/demo.js
@@ -1,4 +1,10 @@
 window.onload = function() {
+	//Add an event listener using on() to all matched elements
+	/*$('body > button')eq(1).on('click', function() {
+		alert('Button clicked!');
+	});*/
+
+	//Add an event listener using on() to the second matched element
 	$('body > button')eq(1).on('click', function() {
 		alert('Button clicked!');
 	});
