2118d38e6fbac1589a8e3c2a8c175eca7cc481fc
rubeewoo <rugoldfishwu@gmail.com>
2021-11-26 10:51:48 +0800

    11-3

--- examples/11-3/demo.js
+++ examples/11-3/demo.js
@@ -10,7 +10,17 @@ window.onload = function() {
 	let lastPosJS = emLengthJS - 1;
 	console.log(psJS[lastPosJS]);
 
-	//jQuery
+	//jQuery, eq()= equal or equivalent
 	let psJQ = $('body > p');
-	console.log(psJQ. eq(-1));
+	//console.log(psJQ. eq(-1)); //Negative number = counting from the end
+	//console.log(psJQ.eq(0)); // 0 or Positive number = counting from the beginning
+
+	//Get HTML frpm an element: innerHTML vs. html()
+	//console.log(psJS[lastPosJS].innerHTML); // Plain JavaScript
+	//console.log(psJQ.eq(-1).html()); //jQuery
+	//console.log(psJQ.eq(-1).innerHTML); //Mixing jQuery with JavaScript = 'undefined'
+
+	//Change the HTML contents of an element
+	//psJS[lastPosJS].innerHTML = 'Content Changed!'; //Plain JavaScript
+	psJQ.eq(-1).html('Content Changed!'); //jQuery
 };
