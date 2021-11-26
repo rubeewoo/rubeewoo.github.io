1af1eaa62c86e7c2514379dc0598b826495923f8
rubeewoo <rugoldfishwu@gmail.com>
2021-11-26 11:25:13 +0800

    Update demo.js

--- examples/11-4/demo.js
+++ examples/11-4/demo.js
@@ -8,4 +8,10 @@ window.onload = function() {
 	$('body > button')eq(1).on('click', function() {
 		alert('Button clicked!');
 	});
+
+	$('body > input').eq(0).on('keypress', function() {
+		let inputVal = $(this).val(); // val()= value
+		let textarea = $('body > textarea').eq(0);
+		textarea.html(inputVal);
+	});
 };
