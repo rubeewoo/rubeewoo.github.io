cee976f5a3a8497fc24e9f7e9092356653d70786
rubeewoo <rugoldfishwu@gmail.com>
2021-11-26 10:10:07 +0800

    Update demo.js

--- examples/11-2/demo.js
+++ examples/11-2/demo.js
@@ -1,5 +1,11 @@
+//let ps = $('body > p');
+//console.log(ps);
+
 window.onload = function() {
 	//$ = jQuery, $()= document.querySelectorAll()
 	let ps= $('body>p');
 	console.log(ps);
+
+	let firstP = $('body > p#first');
+	console.log(firstP);
 };
