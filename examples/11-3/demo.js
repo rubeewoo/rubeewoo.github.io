e0ff1153bef6763b7445340d7a44d87f66fb9480
rubeewoo <rugoldfishwu@gmail.com>
2021-11-26 10:29:23 +0800

    11-3

--- examples/11-3/demo.js
+++ examples/11-3/demo.js
@@ -1,5 +1,16 @@
 window.onload = function() {
 	//Mixing jQuery codes with JavaScript codes:Bad!
 	let ps = $('body > p');
-	console.log(ps[0]);
+	//console.log(ps[0]);
+
+	//Get the last element in JavaScript and jQuery
+	//Plain JavaScript
+	let psJS= document.querySelectorAll('body > p')
+	let emLengthJS = psJS.length;
+	let lastPosJS = emLengthJS - 1;
+	console.log(psJS[lastPosJS]);
+
+	//jQuery
+	let psJQ = $('body > p');
+	console.log(psJQ. eq(-1));
 };
