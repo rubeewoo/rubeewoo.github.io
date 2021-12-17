b75842cd5e22facd73ccfae19e19e7c887efa641
rubeewoo <rugoldfishwu@gmail.com>
2021-12-17 11:05:10 +0800

    `

--- analysis/js/demo.js
+++ analysis/js/demo.js
@@ -1,9 +1,11 @@
 var menuOpen = false; // The default state of the floating menu
+
 window.onload = function () {
 	$('nav > a:first-of-type').on('click',, function(event) {
 		event.preventDefault(); //Stop the navigating default action of a hyperlink.
-		//Take all the non-first <a> elements under <nav> and apply 
+		//Get all the non-first <a> elements under <nav> and apply 
 		let menuItems = $('nav > a:not(:first-of-type');
+		//Set up a variable for creating a different length of animation delay
 		let delay = 0;
 		if(menuOpen) {
 
