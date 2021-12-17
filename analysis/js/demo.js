495ddf7bb7800936a71babac2d7c4fdb7aa5e1b8
rubeewoo <rugoldfishwu@gmail.com>
2021-12-17 10:02:19 +0800

    Update demo.js

--- analysis/js/demo.js
+++ analysis/js/demo.js
@@ -2,6 +2,6 @@ window.onload = function () {
 	$('nav > a:first-of-type').on('click',, function(event) {
 		event.preventDefault(); //Stop the navigating default action of a hyperlink.
 		//Take all the non-first <a> elements under <nav> and apply 
-		$('nav > a:not(first-of-type)').toggle();
+		$('nav > a:not(first-of-type)').toggle(600);
 	});
 };
